6446c67bccf56b014c24bec67143f602e0f8a69f
Zanchi <matteo.zanchi@accenture.com>
2017-06-12 15:32:01 +0200

    .

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
-	if city is None:
+    if city is None:
         return None
 
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
